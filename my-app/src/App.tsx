34b6991e4d8f00482d76a8dba7cbf450ed6db6f9
rikeda-cloud <longsichitian648@gmail.com>
2025-07-06 17:53:54 +0900

    [update] App.tsxを少しカスタマイズ

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,25 +2,22 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const TodoList = [
+	{ id: 1, task: "abc", complated: true },
+	{ id: 2, task: "def", complated: false },
+	{ id: 3, task: "ghi", complated: true },
+	{ id: 4, task: "jkl", complated: false },
+]
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<ul>
+			{TodoList.map((todo) => (
+				<li key={todo.id}>{todo.task} {todo.complated}</li>
+			))}
+		</ul>
+
+	);
 }
 
 export default App;

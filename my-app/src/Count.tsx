14acea1900691a47de147450f4eca0af35882d5d
rikeda-cloud <longsichitian648@gmail.com>
2025-07-08 14:34:59 +0900

    [update] CountPropsの型指定を簡略化

--- my-app/src/Count.tsx
+++ my-app/src/Count.tsx
@@ -1,22 +1,20 @@
-import React from 'react';
-
 // propsの型定義
 interface CountProps {
-  count: number;
-  setCount: React.Dispatch<React.SetStateAction<number>>;
+	count: number;
+	setCount: (updater: number | ((prev: number) => number)) => void;
 }
 
 export function Count({ count, setCount }: CountProps) {
-  // カウントをインクリメントする関数をイベントハンドラーとして定義
-  const handleIncrement = () => {
-    setCount(prevCount => prevCount + 1);
-  };
+	// カウントをインクリメントする関数をイベントハンドラーとして定義
+	const handleIncrement = () => {
+		setCount(prevCount => prevCount + 1);
+	};
 
-  return (
-    <div>
-      <p>現在のカウント: {count}</p>
-      {/* ボタンクリックでhandleIncrement関数を呼び出す */}
-      <button onClick={handleIncrement}>カウントを増やす</button>
-    </div>
-  );
+	return (
+		<div>
+			<p>現在のカウント: {count}</p>
+			{/* ボタンクリックでhandleIncrement関数を呼び出す */}
+			<button onClick={handleIncrement}>カウントを増やす</button>
+		</div>
+	);
 }

32835208dda86198bc8c3d698ed8c516556fa0df
rikeda-cloud <longsichitian648@gmail.com>
2025-07-14 18:24:47 +0900

    [update] HeaderコンポーネントをClick時に任意のpathにルーティング

--- typing-game/src/components/Header.tsx
+++ typing-game/src/components/Header.tsx
@@ -1,12 +1,19 @@
 "use client";
 
 import Image from 'next/image';
+import { useRouter } from "next/navigation";
 
-function Header() {
+interface HeaderProps {
+  path?: string;
+};
+
+function Header({ path = "/" }: HeaderProps) {
+  const router = useRouter();
+
   return (
     <header className="h-1/6 w-full flex items-center justify-start relative">
       <div className="relative w-[150px] h-[40px]">
-        <Image src="/logo.svg" alt="Logo" fill style={{ objectFit: 'contain' }} />
+        <Image src="/logo.svg" alt="Logo" fill style={{ objectFit: 'contain' }} onClick={() => router.push(path)} />
       </div>
     </header>
   );

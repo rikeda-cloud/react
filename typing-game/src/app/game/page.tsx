7059b843c42ac07920943d75f5b2b305971cc649
rikeda-cloud <longsichitian648@gmail.com>
2025-07-16 18:01:10 +0900

    [refactor] Game.tsxコンポーネントを削除し、/gameのpage.tsxに処理を移動

--- typing-game/src/app/game/page.tsx
+++ typing-game/src/app/game/page.tsx
@@ -1,13 +1,28 @@
 "use client";
 
-import Game from "@/components/game/Game";
+import React from 'react';
+import { useTypingGame } from "@/components/useTypingGame";
 import Header from "@/components/Header";
+import ProgressBar from '@/components/ProgressBar';
 
 function GamePage() {
+  const { text, romaji, idx, progress } = useTypingGame();
+
   return (
     <div className="relative flex flex-col items-center justify-center min-h-screen">
       <Header />
-      <Game />
+
+      <div className="text-center w-full max-w-4xl px-4">
+        <div className="mb-8">
+          <ProgressBar progress={progress} />
+        </div>
+
+        <p className="text-4xl mb-4 font-bold">{text}</p>
+        <p className="text-2xl font-mono">
+          <span style={{ color: "#BAC521" }}>{romaji?.slice(0, idx)}</span>
+          <span className="text-gray-500">{romaji?.slice(idx)}</span>
+        </p>
+      </div>
     </div>
   );
 }

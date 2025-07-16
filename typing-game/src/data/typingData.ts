809c1054de8404c93eb69c2db481491af34c51a2
rikeda-cloud <longsichitian648@gmail.com>
2025-07-16 14:32:12 +0900

    [add] typingゲームのお題とローマ字を保持する配列を定義

---
+++ typing-game/src/data/typingData.ts
@@ -0,0 +1,35 @@
+export interface TypingQuestion {
+  text: string;
+  romaji: string;
+}
+
+export const typingQuestions: TypingQuestion[] = [
+  {
+    text: "隣の客はよく柿食う客だ",
+    romaji: "tonarinokyakuhayokukakikuukyakuda"
+  },
+  {
+    text: "生麦生米生卵",
+    romaji: "namamuginamagomenamatamago"
+  },
+  {
+    text: "赤巻紙青巻紙黄巻紙",
+    romaji: "akamakigamiaomakigamikimakigami"
+  },
+  {
+    text: "東京特許許可局",
+    romaji: "toukyoutokkyokyokakyoku"
+  },
+  {
+    text: "庭には二羽鶏がいる",
+    romaji: "niwanihaniwatorigairu"
+  },
+  {
+    text: "すもももももももものうち",
+    romaji: "sumomomomomomomomonouchi"
+  },
+  {
+    text: "坊主が屏風に上手に坊主の絵を描いた",
+    romaji: "bouzugabyoubunijouzunibouzunoeokaita"
+  }
+]

870b8d2eab616103bb26cb610ff565f88d81d949
rikeda-cloud <longsichitian648@gmail.com>
2025-07-16 14:33:08 +0900

    [update] 1つのお題をクリアしたら、次のお題を表示

--- typing-game/src/components/Game.tsx
+++ typing-game/src/components/Game.tsx
@@ -1,15 +1,22 @@
 "use client";
 
+import { typingQuestions } from "@/data/typingData";
 import React from 'react';
 
+function getRandomQuestion() {
+  const randomIdx = Math.floor(Math.random() * typingQuestions.length);
+  return typingQuestions[randomIdx];
+}
+
 function Game() {
-  const text = "隣の客はよく柿食う客だ";
-  const ans = "abc";
+  const [currentQuestion, setCurrentQuestion] =
+    React.useState(getRandomQuestion());
   const [idx, setIdx] = React.useState(0);
 
+  // keyが押された場合に動くハンドラーをセット
   React.useEffect(() => {
     const handleKeyDown = (e: KeyboardEvent) => {
-      if (ans[idx] === e.key) {
+      if (currentQuestion.romaji[idx] === e.key) {
         setIdx((prevIdx) => prevIdx + 1);
       }
     };
@@ -19,12 +26,20 @@ function Game() {
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
     };
-  }, [idx]);
+  }, [idx, currentQuestion]);
+
+  // 問題をクリアしたら次の問題へ
+  React.useEffect(() => {
+    if (idx === currentQuestion.romaji.length) {
+      setCurrentQuestion(getRandomQuestion());
+      setIdx(0);
+    }
+  }, [idx, currentQuestion]);
 
   return (
     <>
-      <p>{text}</p>
-      <p>{idx === 0 ? "" : ans.slice(0, idx)}</p>
+      <p>{currentQuestion.text}</p>
+      <p>{idx === 0 ? "" : currentQuestion.romaji.slice(0, idx)}</p>
     </>
   )
 }

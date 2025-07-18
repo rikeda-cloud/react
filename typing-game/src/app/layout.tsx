b7f2c27810f76cb26ebb4cf7f2425b4f17a2984c
rikeda-cloud <longsichitian648@gmail.com>
2025-07-18 14:17:42 +0900

    [update] layout.tsxにStoreを使うための設定を追加

--- typing-game/src/app/layout.tsx
+++ typing-game/src/app/layout.tsx
@@ -1,21 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import StoreProvider from "@/components/StoreProvider";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-const inter = Inter({
-  variable: "--font-inter",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,11 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <StoreProvider>{children}</StoreProvider>
       </body>
     </html>
   );
 }
+

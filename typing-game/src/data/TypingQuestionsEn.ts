23b6f943b22b79332fa458086b31ca9b4b4ad2cd
rikeda-cloud <longsichitian648@gmail.com>
2025-07-18 13:29:21 +0900

    [add] TypingQuestionsEn.ts

---
+++ typing-game/src/data/TypingQuestionsEn.ts
@@ -0,0 +1,116 @@
+import type { TypingQuestion } from "./TypingQuestion";
+
+export const typingQuestionsEn: TypingQuestion[] = [
+  {
+    text: "hello world",
+    romaji: "helloworld",
+  },
+  {
+    text: "the quick brown fox jumps over the lazy dog",
+    romaji: "thequickbrownfoxjumpsoverthelazydog",
+  },
+  {
+    text: "practice makes perfect",
+    romaji: "practicemakesperfect",
+  },
+  {
+    text: "a piece of cake",
+    romaji: "apieceofcake",
+  },
+  {
+    text: "bite the bullet",
+    romaji: "bitethebullet",
+  },
+  {
+    text: "break a leg",
+    romaji: "breakaleg",
+  },
+  {
+    text: "cutting corners",
+    romaji: "cuttingcorners",
+  },
+  {
+    text: "get out of hand",
+    romaji: "getoutofhand",
+  },
+  {
+    text: "go down in flames",
+    romaji: "godowninflames",
+  },
+  {
+    text: "hang in there",
+    romaji: "hanginthere",
+  },
+  {
+    text: "let the cat out of the bag",
+    romaji: "letthecatoutofthebag",
+  },
+  {
+    text: "make a long story short",
+    romaji: "makealongstoryshort",
+  },
+  {
+    text: "miss the boat",
+    romaji: "misstheboat",
+  },
+  {
+    text: "on the ball",
+    romaji: "ontheball",
+  },
+  {
+    text: "pull someone's leg",
+    romaji: "pullsomeonesleg",
+  },
+  {
+    text: "so far so good",
+    romaji: "sofarsogood",
+  },
+  {
+    text: "speak of the devil",
+    romaji: "speakofthedevil",
+  },
+  {
+    text: "the best of both worlds",
+    romaji: "thebestofbothworlds",
+  },
+  {
+    text: "time is money",
+    romaji: "timeismoney",
+  },
+  {
+    text: "under the weather",
+    romaji: "undertheweather",
+  },
+  {
+    text: "you can say that again",
+    romaji: "youcansaythatagain",
+  },
+  {
+    text: "your guess is as good as mine",
+    romaji: "yourguessisasgoodasmine",
+  },
+  {
+    text: "a bird in the hand is worth two in the bush",
+    romaji: "abirdinthehandisworthtwointhebush",
+  },
+  {
+    text: "a penny for your thoughts",
+    romaji: "apennyforyourthoughts",
+  },
+  {
+    text: "actions speak louder than words",
+    romaji: "actionsspeaklouderthanwords",
+  },
+  {
+    text: "an apple a day keeps the doctor away",
+    romaji: "anappleadaykeepsthedoctoraway",
+  },
+  {
+    text: "curiosity killed the cat",
+    romaji: "curiositykilledthecat",
+  },
+  {
+    text: "every cloud has a silver lining",
+    romaji: "everycloudhasasilverlining",
+  },
+];
